f640152c6d2d6425e053c6b8a0149e0e56aa0c43
kanekiNongMo <1165201419@qq.com>
2018-12-29 14:18:28 +0800

    修改log注释

--- src/main/java/com/kaneki/log/LogAnnotation.java
+++ src/main/java/com/kaneki/log/LogAnnotation.java
@@ -8,6 +8,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.PARAMETER,ElementType.METHOD})// 方法注解
 @Retention(RetentionPolicy.RUNTIME)// 运行时可见
 
+//自定义注解
 public @interface LogAnnotation {
 	String operateType();
  
